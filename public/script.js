e50aefb9f8d45690530e9d6df1f3d207e1d01d49
esmarimassss <esmarimassss@gmail.com>
2026-05-08 12:34:56 +0700

    new ui

--- public/script.js
+++ public/script.js
@@ -1,17 +1,42 @@
-async function loadCount() {
-  let res = await fetch('/api/count');
-  let data = await res.json();
-
-  document.getElementById('count').innerText = data.total;
+async function updateStock(){
+  const res = await fetch('/stock');
+  const data = await res.json();
+  document.getElementById('stock').innerText = data.stock;
 }
 
-async function ambil(count) {
-  let res = await fetch('/api/random/' + count);
-  let data = await res.json();
+async function getNumber(amount){
 
-  document.getElementById('result').value = data.join('\n');
+  const res = await fetch(`/get-number/${amount}`);
+  const data = await res.json();
 
-  loadCount();
+  const result = document.getElementById('result');
+  result.innerHTML = '';
+
+  if(data.numbers.length === 0){
+    result.innerHTML = '<div class="number">Nomor habis</div>';
+    return;
+  }
+
+  data.numbers.forEach(num => {
+
+    const div = document.createElement('div');
+    div.className = 'number';
+
+    div.innerHTML = `
+      ${num}
+      <div class="copy">Klik untuk salin</div>
+    `;
+
+    div.onclick = () => {
+      navigator.clipboard.writeText(num);
+      div.querySelector('.copy').innerText = 'Tersalin';
+    };
+
+    result.appendChild(div);
+
+  });
+
+  updateStock();
 }
 
-loadCount();
+updateStock();
